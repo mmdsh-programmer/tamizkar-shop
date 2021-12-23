191193a472d275f9c8f0024dda323e3e107f08ec
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-12-23 17:44:37 +0330

    change logo size

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Header() {
   return (
     <header className="w-full absolute left-0 top-0 z-10">
-      <div className="container mx-auto py-6">
+      <div className="container mx-auto py-6 md:py-12">
         <img
-          className="mx-auto w-32 h-auto object-cover"
+          className="mx-auto w-40 md:w-80 h-auto object-cover"
           src="/images/logo.webp"
           alt="tamzkar shop logo"
         />
